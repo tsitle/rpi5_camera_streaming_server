bd8d43e25031a4e485c405e131949f0ef67d46f5
Thomas <technisandk@gmail.com>
2023-09-10 19:29:19 +0200

    Add opencv build dir

---
+++ build_opencv/build-opencv-rpi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#
+# by TS, Aug 2023
+#
+
+COMPILE_WITH_GUI=false
+COMPILE_WITH_FFMPEG=true
+COMPILE_WITH_GSTREAMER=true
+NEED_LIBCAMERA=true  # only Raspberry Pi
+
+. .build-opencv.sh
