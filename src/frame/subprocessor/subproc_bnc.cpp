04d0b61ff7d7b1d83cf87592af215ab003bef909
Thomas <technisandk@gmail.com>
2023-09-16 00:49:37 +0200

    Fix FrameSubProc BNC

--- src/frame/subprocessor/subproc_bnc.cpp
+++ src/frame/subprocessor/subproc_bnc.cpp
@@ -22,6 +22,15 @@ namespace framesubproc {
 	}
 
 	void FrameSubProcessorBrightnAndContrast::processFrame(cv::Mat &frame) {
+		if (gOptBrightness == 0 && gOptContrast == 0) {
+			return;
+		}
+
+		cv::Mat *pContrFrameInp = &frame;
+		cv::Mat brightnFrameOut;
+		cv::Mat contrFrameOut;
+		cv::Mat *pFrameOut = &brightnFrameOut;
+
 		if (gOptBrightness != 0) {
 			int16_t shadow = gOptBrightness;
 			int16_t highlight = 255;
@@ -35,16 +44,29 @@ namespace framesubproc {
 			double alpha_b = (double)(highlight - shadow) / 255.0;
 			double gamma_b = (double)shadow;
 			
-			cv::addWeighted(frame, alpha_b, frame, 0.0, gamma_b, frame);
+			/**
+			 * gOptBrightness = 23
+			 * --> a=0.909804, g=23
+			 */
+			/**std::cout << "B a=" << alpha_b << ", g=" << gamma_b << std::endl;**/
+			cv::addWeighted(frame, alpha_b, frame, 0.0, gamma_b, *pFrameOut);
+			pContrFrameInp = pFrameOut;
 		}
-		
 		if (gOptContrast != 0) {
 			double f = 131.0 * (double)(gOptContrast + 127) / (double)(127 * (131 - gOptContrast));
 			double alpha_c = f;
 			double gamma_c = 127.0 * (double)(1.0 - f);
 			
-			cv::addWeighted(frame, alpha_c, frame, 0.0, gamma_c, frame);
+			/**
+			 * gOptContrast = 5
+			 * --> a=1.08061, g=-10.2381
+			 */
+			/**std::cout << "C a=" << alpha_c << ", g=" << gamma_c << std::endl;**/
+			cv::addWeighted(*pContrFrameInp, alpha_c, *pContrFrameInp, 0.0, gamma_c, contrFrameOut);
+			pFrameOut = &contrFrameOut;
 		}
+		//
+		frame = pFrameOut->clone();
 	}
 
 }  // namespace framesubproc
