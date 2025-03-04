902160fe11bffcd89d3f128fac3017200166e624
Thomas <technisandk@gmail.com>
2025-03-04 17:06:08 +0100

    Add build_opencv/4.11.0/

---
+++ build_opencv/4.11.0/build-opencv-client.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#
+# by TS, Aug 2023
+#
+
+COMPILE_WITH_GUI=true
+COMPILE_WITH_FFMPEG=true
+COMPILE_WITH_GSTREAMER=true
+NEED_LIBCAMERA=false  # only Raspberry Pi
+
+. .build-opencv.sh
---
+++ build_opencv/4.11.0/build-opencv-rpi.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#
+# by TS, Aug 2023
+#
+
+COMPILE_WITH_GUI=false
+COMPILE_WITH_FFMPEG=true
+COMPILE_WITH_GSTREAMER=true
+NEED_LIBCAMERA=true  # only Raspberry Pi
+
+. .build-opencv.sh
