36c08d11e25ec58ea005e0747bdb31d2e78c4e3f
Thomas <technisandk@gmail.com>
2025-03-04 14:28:42 +0100

    run.sh: Add note regarding libcamera GStreamer plugin

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-#CUSTOM_GSTREAMER_PLUGIN_PATH="~/test-opencv_mpjeg_stream-cpp/build_libcamera/libcamera/build/src/gstreamer"
-CUSTOM_GSTREAMER_PLUGIN_PATH="/usr/local/lib/arm-linux-gnueabihf/gstreamer-1.0/libgstlibcamera.so"
-export GST_PLUGIN_PATH="$CUSTOM_GSTREAMER_PLUGIN_PATH"
+# Starting with libcamera-0.4 there should be no need for building a custom
+# version of the libcamera Gstreamer plugin anymore.
+# Support for setting the Autofocus Mode is now implemented in the official release
+# of the library.
+# It can now be set in a GStreamer pipeline like this: 'libcamerasrc af-mode=continuous [...]'
+exportCustomBuiltGstreamerPluginPath() {
+	#CUSTOM_GSTREAMER_PLUGIN_PATH="~/test-opencv_mpjeg_stream-cpp/build_libcamera/libcamera/build/src/gstreamer"
+	CUSTOM_GSTREAMER_PLUGIN_PATH="/usr/local/lib/arm-linux-gnueabihf/gstreamer-1.0/libgstlibcamera.so"
+	export GST_PLUGIN_PATH="$CUSTOM_GSTREAMER_PLUGIN_PATH"
+}
+#exportCustomBuiltGstreamerPluginPath
 
 ./comp.sh || exit 1
 
