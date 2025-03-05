5ce1694254ecf856f5f834368ba17bc6b864d92a
Thomas <technisandk@gmail.com>
2025-03-05 10:27:07 +0100

    .build-opencv.sh: Add basic support for macOS

--- build_opencv/4.11.0/.build-opencv.sh
+++ build_opencv/4.11.0/.build-opencv.sh
@@ -4,110 +4,216 @@
 # https://docs.opencv.org/4.x/db/d05/tutorial_config_reference.html
 #
 
-[[ -z "$COMPILE_WITH_GUI" ]] && {
-	echo "Missing COMPILE_WITH_GUI. Aborting." >>/dev/stderr
-	exit 1
-}
-[[ -z "$COMPILE_WITH_FFMPEG" ]] && {
-	echo "Missing COMPILE_WITH_FFMPEG. Aborting." >>/dev/stderr
-	exit 1
-}
-[[ -z "$COMPILE_WITH_GSTREAMER" ]] && {
-	echo "Missing COMPILE_WITH_GSTREAMER. Aborting." >>/dev/stderr
-	exit 1
-}
-[[ -z "$NEED_LIBCAMERA" ]] && {
-	echo "Missing NEED_LIBCAMERA. Aborting." >>/dev/stderr
-	exit 1
-}
-
-OPENCV_RELEASE="opencv-4.11.0"
+VAR_MYNAME="$(basename "$0")"
 
 #
 
-if [ ! -f "${OPENCV_RELEASE}.tar.gz" ]; then
-	echo "You'll need to download '${OPENCV_RELEASE}.tar.gz' from 'https://github.com/opencv/opencv/releases' first." >>/dev/stderr
+[[ -z "${COMPILE_WITH_GUI}" ]] && {
+	echo "${VAR_MYNAME}: Missing COMPILE_WITH_GUI. Aborting." >>/dev/stderr
+	exit 1
+}
+[[ -z "${COMPILE_WITH_FFMPEG}" ]] && {
+	echo "${VAR_MYNAME}: Missing COMPILE_WITH_FFMPEG. Aborting." >>/dev/stderr
+	exit 1
+}
+[[ -z "${COMPILE_WITH_GSTREAMER}" ]] && {
+	echo "${VAR_MYNAME}: Missing COMPILE_WITH_GSTREAMER. Aborting." >>/dev/stderr
+	exit 1
+}
+[[ -z "${NEED_LIBCAMERA}" ]] && {
+	echo "${VAR_MYNAME}: Missing NEED_LIBCAMERA. Aborting." >>/dev/stderr
+	exit 1
+}
+
+LCFG_OPENCV_RELEASE="opencv-4.11.0"
+
+#
+
+# Outputs Operating System name
+#
+# @return int EXITCODE
+function getOsName() {
+	case "${OSTYPE}" in
+		linux*)
+			echo -n "linux"
+			;;
+		darwin*)
+			echo -n "macos"
+			;;
+		*)
+			echo "${VAR_MYNAME}: Error: getOsName(): Unknown OSTYPE '${OSTYPE}'" >>/dev/stderr
+			return 1
+			;;
+	esac
+	return 0
+}
+
+getOsName >/dev/null || exit 1
+
+LVAR_OSNAME="$(getOsName)"
+
+#
+
+if [ ! -f "${LCFG_OPENCV_RELEASE}.tar.gz" ]; then
+	echo "${VAR_MYNAME}: You'll need to download '${LCFG_OPENCV_RELEASE}.tar.gz' from 'https://github.com/opencv/opencv/releases' first." >>/dev/stderr
 	exit 1
 fi
 
-sudo apt-get update || exit 1
-sudo apt-get install -y \
-		g++ \
-		cmake \
-		make \
-		git \
-		pkg-config \
-		libgnutls28-dev \
-		libv4l-dev \
-		|| exit 1
+case "${LVAR_OSNAME}" in
+	linux)
+		echo "${VAR_MYNAME}: running 'apt-get update'"
+		sudo apt-get update || exit 1
+		echo "${VAR_MYNAME}: running 'apt-get install [...]'"
+		sudo apt-get install -y \
+				g++ \
+				cmake \
+				make \
+				git \
+				pkg-config \
+				libgnutls28-dev \
+				libv4l-dev \
+				|| exit 1
+		;;
+	macos)
+		#echo "${VAR_MYNAME}: running 'brew install [...]'"
+		#brew install \
+		#		cmake \
+		#		|| exit 1
+		;;
+	*)
+		echo "${VAR_MYNAME}: Error: Unknown OSTYPE '$OSTYPE'" >>/dev/stderr
+		exit 1
+		;;
+esac
 
-if [ ! -d "${OPENCV_RELEASE}" ]; then
-	tar xf "${OPENCV_RELEASE}.tar.gz" || exit 1
+if [ ! -d "${LCFG_OPENCV_RELEASE}" ]; then
+	tar xf "${LCFG_OPENCV_RELEASE}.tar.gz" || exit 1
 fi
 
 test -d build && rm -fr build
 mkdir build && cd build
 
 TMP_OPT_HIGHGUI="OFF"
-if [ "$COMPILE_WITH_GUI" = "true" ]; then
+if [ "${COMPILE_WITH_GUI}" = "true" ]; then
 	TMP_OPT_HIGHGUI="ON"
-	sudo apt install -y libgtk2.0-dev || exit 1
+
+	case "${LVAR_OSNAME}" in
+		linux)
+			echo "${VAR_MYNAME}: running 'apt-get install [...]'"
+			sudo apt-get install -y \
+					libgtk2.0-dev \
+					|| exit 1
+			;;
+		macos)
+			;;
+	esac
 fi
 
 TMP_OPT_FFMPEG="OFF"
-if [ "$COMPILE_WITH_FFMPEG" = "true" ]; then
+if [ "${COMPILE_WITH_FFMPEG}" = "true" ]; then
 	TMP_OPT_FFMPEG="ON"
-	# libavresample-dev doesn't seem to exist anymore. Replacing it with libswresample-dev
-	# libavresample4 doesn't seem to exist anymore. Replacing it with libswresample4
-	sudo apt-get install -y \
-			libavcodec-dev \
-			libavformat-dev \
-			libavutil-dev \
-			libswscale-dev \
-			libswresample-dev \
-			libswresample4 \
-			libjpeg-dev \
-			libopenjp2-7-dev \
-			libpng-dev \
-			|| exit 1
+
+	case "${LVAR_OSNAME}" in
+		linux)
+			echo "${VAR_MYNAME}: running 'apt-get install [...]'"
+			# libavresample-dev doesn't seem to exist anymore. Replacing it with libswresample-dev
+			# libavresample4 doesn't seem to exist anymore. Replacing it with libswresample4
+			sudo apt-get install -y \
+					libavcodec-dev \
+					libavformat-dev \
+					libavutil-dev \
+					libswscale-dev \
+					libswresample-dev \
+					libswresample4 \
+					libjpeg-dev \
+					libopenjp2-7-dev \
+					libpng-dev \
+					|| exit 1
+			;;
+		macos)
+			;;
+	esac
 fi
 
 TMP_OPT_GSTREAMER="OFF"
-if [ "$COMPILE_WITH_GSTREAMER" = "true" ]; then
+if [ "${COMPILE_WITH_GSTREAMER}" = "true" ]; then
 	TMP_OPT_GSTREAMER="ON"
-	sudo apt-get install -y \
-			libgstreamer1.0-dev \
-			libgstreamer-plugins-base1.0-dev \
-			libgstreamer-plugins-bad1.0-dev \
-			gstreamer1.0-plugins-base \
-			gstreamer1.0-plugins-base-apps \
-			gstreamer1.0-plugins-good \
-			gstreamer1.0-plugins-bad \
-			gstreamer1.0-plugins-ugly \
-			gstreamer1.0-libav \
-			gstreamer1.0-tools \
-			gstreamer1.0-alsa \
-			gstreamer1.0-gl \
-			gstreamer1.0-pulseaudio \
-			|| exit 1
-	if [ "$COMPILE_WITH_GUI" = "true" ]; then
-		sudo apt install -y \
-				gstreamer1.0-x \
-				gstreamer1.0-gtk3 \
-				gstreamer1.0-qt5 \
-				|| exit 1
+
+	case "${LVAR_OSNAME}" in
+		linux)
+			echo "${VAR_MYNAME}: running 'apt-get install [...]'"
+			sudo apt-get install -y \
+					libgstreamer1.0-dev \
+					libgstreamer-plugins-base1.0-dev \
+					libgstreamer-plugins-bad1.0-dev \
+					gstreamer1.0-plugins-base \
+					gstreamer1.0-plugins-base-apps \
+					gstreamer1.0-plugins-good \
+					gstreamer1.0-plugins-bad \
+					gstreamer1.0-plugins-ugly \
+					gstreamer1.0-libav \
+					gstreamer1.0-tools \
+					gstreamer1.0-alsa \
+					gstreamer1.0-gl \
+					gstreamer1.0-pulseaudio \
+					|| exit 1
+			if [ "${COMPILE_WITH_GUI}" = "true" ]; then
+				echo "${VAR_MYNAME}: running 'apt-get install [...]'"
+				sudo apt-get install -y \
+						gstreamer1.0-x \
+						gstreamer1.0-gtk3 \
+						gstreamer1.0-qt5 \
+						|| exit 1
+			fi
+			;;
+		macos)
+			;;
+	esac
+fi
+
+if [ "${NEED_LIBCAMERA}" = "true" ]; then
+	case "${LVAR_OSNAME}" in
+		linux)
+			echo "${VAR_MYNAME}: running 'apt-get install [...]'"
+			sudo apt-get install -y \
+					libcamera-dev \
+					|| exit 1
+			;;
+		macos)
+			;;
+	esac
+fi
+
+LVAR_BIN_CMAKE=""
+if command -v cmake >/dev/null; then
+	LVAR_BIN_CMAKE="cmake"
+else
+	LTMP_OS="$(getOsName)"
+	if [ "${LTMP_OS}" = "macos" -a -x "${HOME}/Applications/CLion.app/Contents/bin/cmake/mac/x64/bin/cmake" ]; then
+		LVAR_BIN_CMAKE="${HOME}/Applications/CLion.app/Contents/bin/cmake/mac/x64/bin/cmake"
+	elif [ "${LTMP_OS}" = "linux" ]; then
+		TMP_FIND_FN="tmp.find-$$.tmp"
+		test -f "${TMP_FIND_FN}" && rm "${TMP_FIND_FN}"
+		find /opt -maxdepth 1 -type d -name "clion-*" | sort -r > "${TMP_FIND_FN}"
+		while IFS= read -r TMP_DN; do
+			LVAR_BIN_CMAKE="$(find "${TMP_DN}" -type f -name "cmake" -perm -u=x 2>/dev/null)"
+			break
+		done < "${TMP_FIND_FN}"
+		rm "${TMP_FIND_FN}"
 	fi
 fi
 
-if [ "$NEED_LIBCAMERA" = "true" ]; then
-	sudo apt-get install -y libcamera-dev || exit 1
+if [ -z "${LVAR_BIN_CMAKE}" ]; then
+	echo "${VAR_MYNAME}: Couldn't find cmake. Aborting." >>/dev/stderr
+	exit 1
 fi
 
-cmake \
+echo "${VAR_MYNAME}: running 'cmake [...]'"
+${LVAR_BIN_CMAKE} \
 		-D WITH_LIBV4L=ON \
 		-D WITH_V4L=ON \
-		-D WITH_FFMPEG=$TMP_OPT_FFMPEG \
-		-D WITH_GSTREAMER=$TMP_OPT_GSTREAMER \
+		-D WITH_FFMPEG=${TMP_OPT_FFMPEG} \
+		-D WITH_GSTREAMER=${TMP_OPT_GSTREAMER} \
 		-D WITH_MSMF=OFF \
 		-D WITH_DSHOW=OFF \
 		-D WITH_AVFOUNDATION=OFF \
@@ -118,16 +224,27 @@ cmake \
 		-DBUILD_PERF_TESTS=OFF \
 		-DBUILD_opencv_calib3d=ON \
 		-DBUILD_opencv_dnn=OFF \
-		-DBUILD_opencv_highgui=$TMP_OPT_HIGHGUI \
+		-DBUILD_opencv_highgui=${TMP_OPT_HIGHGUI} \
 		-DBUILD_opencv_java=OFF \
 		-DBUILD_opencv_js=OFF \
 		-DBUILD_opencv_ml=OFF \
 		-DBUILD_opencv_objc=OFF \
 		-DBUILD_opencv_python=OFF \
 		-DOPENCV_FORCE_LIBATOMIC_COMPILER_CHECK=1 \
-		"../$OPENCV_RELEASE" || exit 1
+		"../${LCFG_OPENCV_RELEASE}" || exit 1
 
 #
 
+echo "${VAR_MYNAME}: running 'make [...]'"
 make -j4 || exit 1
-sudo make install
+
+echo
+case "${LVAR_OSNAME}" in
+	linux)
+		echo "${VAR_MYNAME}: running 'make install'"
+		sudo make install
+		;;
+	macos)
+		echo "${VAR_MYNAME}: Not installing OpenCV in system"
+		;;
+esac
